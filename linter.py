076d0b63375e7178df16c9f9788b734eba4ac42c
thesave <thesave86@gmail.com>
2015-06-03 21:33:00 +0200

    update

--- linter.py
+++ linter.py
@@ -11,7 +11,9 @@
 """This module exports the jolint plugin class."""
 
 from SublimeLinter.lint import Linter, util
+import sublime
 import os
+import codecs
 
 class JoLint(Linter):
     
@@ -25,7 +27,23 @@ class JoLint(Linter):
     line_col_base = (1, 1)
     tempfile_suffix = None
     error_stream = util.STREAM_BOTH
+    USER_SETTINGS = "SublimeLinter-jolint.sublime-settings"
 
-    if os.environ.get( "JOLIE_HOME" ) is None:
+    if True: #os.environ.get( "JOLIE_HOME" ) is None:
+        path = os.path.join( sublime.packages_path(), "SublimeLinter-jolint" )
+        settings_file = os.path.join( path, USER_SETTINGS )
+        if os.path.exists( path ):
+            if os.path.isfile( settings_file ):
+                sublime.message_dialog("USER_SETTINGS path: " + settings_file )
+                with codecs.open( settings_file, 'r', 'UTF-8') as settings:
+                    settings.readlines()
+            else:
+                sublime.message_dialog("USER_SETTINGS file does not exist, creating" )
+                with codecs.open( settings_file, 'w', 'UTF-8' ) as settings:
+                    settings_file.writelines( { "JOLIE_HOME" : "/usr/lib/jolie" } )
+
         # Set JOLIE_HOME according to your installation path
         env = { "JOLIE_HOME" : "/usr/lib/jolie" }
+    else :
+        sublime.message_dialog("\"JOLIE_HOME\" is set to " )
+
